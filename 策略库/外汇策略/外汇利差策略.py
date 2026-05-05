92f8f7653eaa07b4289c537b0e03209a2b7e572a
jgl890474 <jinguanlin17@gmail.com>
2026-05-05 18:44:21 +0800

    Change import statement to absolute import style

--- 策略库/外汇策略/外汇利差策略.py
+++ 策略库/外汇策略/外汇利差策略.py
@@ -2,11 +2,12 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from ..策略基类 import BaseStrategy
+
+# 方式1：使用绝对导入
+from 策略库.策略基类 import BaseStrategy
 
 class ForexCarryStrategy(BaseStrategy):
-    """外汇利差交易策略"""
-    
+    """外汇利差策略"""
     def on_data(self, kline):
         if self.position == 0:
             return 'buy'
