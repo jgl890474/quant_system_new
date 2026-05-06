f2641c9b5265e460c5c64088855f12ec2b1915da
jgl890474 <jinguanlin17@gmail.com>
2026-05-06 11:53:10 +0800

    Implement logging utility in 日志工具.py
    Add logging functionality to the tool with a customizable logger name.

--- 工具/日志工具.py
+++ 工具/日志工具.py
@@ -1,1 +1,13 @@
 
+# -*- coding: utf-8 -*-
+import logging
+
+def 获取日志(名称="量化系统"):
+    logger = logging.getLogger(名称)
+    logger.setLevel(logging.INFO)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
