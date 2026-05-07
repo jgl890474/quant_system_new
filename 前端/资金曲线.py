f38f28e1729ff809e13a14bcb8192ce69755792d
jgl890474 <jinguanlin17@gmail.com>
2026-05-07 10:44:12 +0800

    Add database snapshot saving for current funds

--- 前端/资金曲线.py
+++ 前端/资金曲线.py
@@ -3,6 +3,7 @@ import streamlit as st
 import plotly.graph_objects as go
 import pandas as pd
 from datetime import datetime, timedelta
+from 工具 import 数据库
 
 def 显示(引擎):
     st.markdown("### 📈 资金曲线")
@@ -15,11 +16,25 @@ def 显示(引擎):
     with col3:
         st.metric("胜率", "65.8%")
     
-    # 生成收益曲线数据
-    日期列表 = pd.date_range(start=datetime.now() - timedelta(days=180), end=datetime.now(), freq='D')
-    资产列表 = [引擎.初始资金 + (引擎.获取总盈亏()) * i / len(日期列表) for i in range(len(日期列表))]
+    # 保存今日资金快照到数据库
+    try:
+        总资产 = 引擎.获取总资产()
+        总盈亏 = 引擎.获取总盈亏()
+        持仓市值 = sum(p.数量 * p.当前价格 for p in 引擎.持仓.values())
+        可用资金 = 总资产 - 持仓市值
+        数据库.保存资金快照(总资产, 总盈亏, 持仓市值, 可用资金)
+    except:
+        pass
     
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=日期列表, y=资产列表, mode='lines', name='资产', line=dict(color='#00d2ff', width=2)))
-    fig.update_layout(height=400, title="资产曲线", paper_bgcolor="#0a0c10", plot_bgcolor="#15171a", font_color="#e6e6e6")
-    st.plotly_chart(fig, use_container_width=True)
+    # 从数据库获取历史资金曲线
+    历史数据 = 数据库.获取资金曲线(180)
+    
+    if not 历史数据.empty:
+        日期 = 历史数据['日期']
+        资产 = 历史数据['总资产']
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=日期, y=资产, mode='lines', name='资产', line=dict(color='#00d2ff', width=2)))
+        fig.update_layout(height=400, title="资产曲线（历史）", paper_bgcolor="#0a0c10", plot_bgcolor="#15171a")
+        st.plotly_chart(fig, use_container_width=True)
+    else:
+        st.info("暂无历史数据，完成交易后将自动记录")
