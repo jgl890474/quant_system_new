e632e20787f7aef7caded86fdf36367f19bc1c54
jgl890474 <jinguanlin17@gmail.com>
2026-05-16 00:44:50 +0800

    Remove comments and unused attributes from 策略基类

--- 核心/策略基类.py
+++ 核心/策略基类.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
-"""策略基类"""
-
 class 策略基类:
     def __init__(self, 名称, 品种, 初始资金):
         self.名称 = 名称
         self.品种 = 品种
         self.初始资金 = 初始资金
-        self.资金 = 初始资金
-        self.持仓 = 0
-
-    def 处理行情(self, k线):
-        return 'hold'
