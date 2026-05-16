a715a63f342dfd9f39fdcf84816d491981d004b1
jgl890474 <jinguanlin17@gmail.com>
2026-05-16 10:47:53 +0800

    Add new strategy class '我的新策略'

--- 策略库/A股策略/简单策略.py
+++ 策略库/A股策略/简单策略.py
@@ -1,1 +1,10 @@
+# -*- coding: utf-8 -*-
+from 核心.策略基类 import 策略基类
 
+class 我的新策略(策略基类):
+    def __init__(self, 名称, 品种, 初始资金):
+        super().__init__(名称, 品种, 初始资金)
+    
+    def 处理行情(self, k线):
+        # 你的策略逻辑
+        return 'hold'
